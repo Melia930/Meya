e2ab9a7384fc2744a2aac187faa4b8e177552296
Melia930 <103120560+Melia930@users.noreply.github.com>
2022-04-06 15:02:19 +0200

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,25 +1,68 @@
 var wms_layers = [];
 
-var format_ZE_OCCITANIE_0 = new ol.format.GeoJSON();
-var features_ZE_OCCITANIE_0 = format_ZE_OCCITANIE_0.readFeatures(json_ZE_OCCITANIE_0, 
+
+        var lyr_OpenStreetMapmonochrome_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap monochrome',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://a.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+            })
+        });
+var format_COMMUNE_1 = new ol.format.GeoJSON();
+var features_COMMUNE_1 = format_COMMUNE_1.readFeatures(json_COMMUNE_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_ZE_OCCITANIE_0 = new ol.source.Vector({
+var jsonSource_COMMUNE_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_ZE_OCCITANIE_0.addFeatures(features_ZE_OCCITANIE_0);
-var lyr_ZE_OCCITANIE_0 = new ol.layer.Vector({
+jsonSource_COMMUNE_1.addFeatures(features_COMMUNE_1);
+var lyr_COMMUNE_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_ZE_OCCITANIE_0, 
-                style: style_ZE_OCCITANIE_0,
+                source:jsonSource_COMMUNE_1, 
+                style: style_COMMUNE_1,
                 interactive: true,
-                title: '<img src="styles/legend/ZE_OCCITANIE_0.png" /> ZE_OCCITANIE'
+    title: 'COMMUNE<br />\
+    <img src="styles/legend/COMMUNE_1_0.png" /> Bazouges-sur-le-Loir<br />\
+    <img src="styles/legend/COMMUNE_1_1.png" /> Bousse<br />\
+    <img src="styles/legend/COMMUNE_1_2.png" /> Clermont-Créans<br />\
+    <img src="styles/legend/COMMUNE_1_3.png" /> Cré<br />\
+    <img src="styles/legend/COMMUNE_1_4.png" /> Crosmières<br />\
+    <img src="styles/legend/COMMUNE_1_5.png" /> La Flèche<br />\
+    <img src="styles/legend/COMMUNE_1_6.png" /> Le Bailleul<br />\
+    <img src="styles/legend/COMMUNE_1_7.png" /> Ligron<br />\
+    <img src="styles/legend/COMMUNE_1_8.png" /> Mareil-sur-Loir<br />\
+    <img src="styles/legend/COMMUNE_1_9.png" /> Villaines-sous-Malicorne<br />\
+    <img src="styles/legend/COMMUNE_1_10.png" /> <br />'
+        });
+var format_CHEF_LIEU_2 = new ol.format.GeoJSON();
+var features_CHEF_LIEU_2 = format_CHEF_LIEU_2.readFeatures(json_CHEF_LIEU_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_CHEF_LIEU_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_CHEF_LIEU_2.addFeatures(features_CHEF_LIEU_2);cluster_CHEF_LIEU_2 = new ol.source.Cluster({
+  distance: 10,
+  source: jsonSource_CHEF_LIEU_2
+});
+var lyr_CHEF_LIEU_2 = new ol.layer.Vector({
+                declutter: true,
+                source:cluster_CHEF_LIEU_2, 
+                style: style_CHEF_LIEU_2,
+                interactive: true,
+                title: '<img src="styles/legend/CHEF_LIEU_2.png" /> CHEF_LIEU'
             });
 
-lyr_ZE_OCCITANIE_0.setVisible(true);
-var layersList = [lyr_ZE_OCCITANIE_0];
-lyr_ZE_OCCITANIE_0.set('fieldAliases', {'ID': 'ID', 'NOM_COM': 'NOM_COM', 'NOM_COM_M': 'NOM_COM_M', 'INSEE_COM': 'INSEE_COM', 'STATUT': 'STATUT', 'POPULATION': 'POPULATION', 'INSEE_CAN': 'INSEE_CAN', 'INSEE_ARR': 'INSEE_ARR', 'INSEE_DEP': 'INSEE_DEP', 'INSEE_REG': 'INSEE_REG', 'CODE_EPCI': 'CODE_EPCI', 'LIBGEO': 'LIBGEO', 'ZE2020': 'ZE2020', 'LIBZE2020': 'LIBZE2020', 'ZE2020_PAR': 'ZE2020_PAR', 'DEP': 'DEP', 'REG': 'REG', 'BE2021': 'BE2021', 'LIBBE2021': 'LIBBE2021', });
-lyr_ZE_OCCITANIE_0.set('fieldImages', {'ID': '', 'NOM_COM': '', 'NOM_COM_M': '', 'INSEE_COM': '', 'STATUT': '', 'POPULATION': '', 'INSEE_CAN': '', 'INSEE_ARR': '', 'INSEE_DEP': '', 'INSEE_REG': '', 'CODE_EPCI': '', 'LIBGEO': '', 'ZE2020': '', 'LIBZE2020': '', 'ZE2020_PAR': '', 'DEP': '', 'REG': '', 'BE2021': '', 'LIBBE2021': '', });
-lyr_ZE_OCCITANIE_0.set('fieldLabels', {'ID': 'inline label', 'NOM_COM': 'inline label', 'NOM_COM_M': 'inline label', 'INSEE_COM': 'inline label', 'STATUT': 'inline label', 'POPULATION': 'inline label', 'INSEE_CAN': 'no label', 'INSEE_ARR': 'no label', 'INSEE_DEP': 'no label', 'INSEE_REG': 'no label', 'CODE_EPCI': 'inline label', 'LIBGEO': 'no label', 'ZE2020': 'header label', 'LIBZE2020': 'inline label', 'ZE2020_PAR': 'no label', 'DEP': 'inline label', 'REG': 'no label', 'BE2021': 'no label', 'LIBBE2021': 'no label', });
-lyr_ZE_OCCITANIE_0.on('precompose', function(evt) {
+lyr_OpenStreetMapmonochrome_0.setVisible(true);lyr_COMMUNE_1.setVisible(true);lyr_CHEF_LIEU_2.setVisible(true);
+var layersList = [lyr_OpenStreetMapmonochrome_0,lyr_COMMUNE_1,lyr_CHEF_LIEU_2];
+lyr_COMMUNE_1.set('fieldAliases', {'ID': 'ID', 'PREC_PLANI': 'PREC_PLANI', 'NOM': 'Nom de la commune', 'CODE_INSEE': 'CODE_INSEE', 'STATUT': 'STATUT', 'CANTON': 'CANTON', 'ARRONDISST': 'ARRONDISST', 'DEPART': 'DEPART', 'REGION': 'REGION', 'POPUL': 'Population', 'MULTICAN': 'MULTICAN', });
+lyr_CHEF_LIEU_2.set('fieldAliases', {'ID': 'ID', 'ID_COM': 'ID_COM', 'ORIGIN_NOM': 'ORIGIN_NOM', 'NATURE': 'NATURE', 'NOM': 'NOM', 'IMPORTANCE': 'IMPORTANCE', });
+lyr_COMMUNE_1.set('fieldImages', {'ID': 'TextEdit', 'PREC_PLANI': 'TextEdit', 'NOM': 'TextEdit', 'CODE_INSEE': 'TextEdit', 'STATUT': 'TextEdit', 'CANTON': 'TextEdit', 'ARRONDISST': 'TextEdit', 'DEPART': 'TextEdit', 'REGION': 'TextEdit', 'POPUL': 'Range', 'MULTICAN': 'TextEdit', });
+lyr_CHEF_LIEU_2.set('fieldImages', {'ID': 'TextEdit', 'ID_COM': 'TextEdit', 'ORIGIN_NOM': 'TextEdit', 'NATURE': 'TextEdit', 'NOM': 'TextEdit', 'IMPORTANCE': 'TextEdit', });
+lyr_COMMUNE_1.set('fieldLabels', {'ID': 'no label', 'PREC_PLANI': 'no label', 'NOM': 'header label', 'CODE_INSEE': 'no label', 'STATUT': 'no label', 'CANTON': 'inline label', 'ARRONDISST': 'inline label', 'DEPART': 'no label', 'REGION': 'inline label', 'POPUL': 'inline label', 'MULTICAN': 'header label', });
+lyr_CHEF_LIEU_2.set('fieldLabels', {'ID': 'no label', 'ID_COM': 'no label', 'ORIGIN_NOM': 'header label', 'NATURE': 'header label', 'NOM': 'header label', 'IMPORTANCE': 'no label', });
+lyr_CHEF_LIEU_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
